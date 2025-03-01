d5b53dc18dfcd4f6bc04502ce436d17c388c7d73
some-du6e <103129948+some-du6e@users.noreply.github.com>
2025-02-28 20:00:35 -0600

    frontend

--- src/manager.js
+++ src/manager.js
@@ -1,7 +1,8 @@
 import fs from "fs";
 
 // Function to read JSON file and convert it to a dictionary
-function jsonToDictionary(filePath) {
+function jsonToDictionary(demure) {
+    const filePath = '../data.json';
     try {
         const data = fs.readFileSync(filePath, 'utf8');
         const dictionary = JSON.parse(data);
@@ -18,6 +19,9 @@ function getall() {
     // Example usage
     const dictionary = jsonToDictionary('data.json');
     console.log(dictionary["games"]);
+}
+function add(name, notes, released) {
+
 }
 
 export { getall };
--- src/www/index.js
+++ src/www/index.js
@@ -2,14 +2,25 @@ function main() {
   console.log('Hello World!');
 }
 function titlestuff() {
-  const titles = ["-__Inventario__", "_-_Inventario__", "__-Inventario___", "___Inventario-__", "___Inventario_-_", "___Inventario__-"];
-let index = 0;
-
-setInterval(() => {
+  const titles = [
+    'Xxx_Inventario_xxx',
+    'xXx_Inventario_xxx',
+    'xxX_Inventario_xxx',
+    'xxx_Inventario_Xxx',
+    'xxx_Inventario_xXx',
+    'xxx_Inventario_xxX',
+  ];
+  let index = 0;
+  const stupidahhtitle = document.getElementById('sigmaboytitle');
+  setInterval(() => {
     document.title = titles[index];
+    stupidahhtitle.innerText = titles[index];
     index = (index + 1) % titles.length;
-}, 500);
+  }, 500);
 }
 
 document.addEventListener('DOMContentLoaded', main);
 document.addEventListener('DOMContentLoaded', titlestuff);
+document.getElementById('add-button').addEventListener('click', function () {
+  alert('sigma');
+});
